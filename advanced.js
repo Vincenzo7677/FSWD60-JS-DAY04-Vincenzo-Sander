f80f880a56616f91fcda329ab7d75e413238e2e7
MrOsteoperosis <SanderVerheijen@outlook.com>
2019-01-23 15:23:07 +0100

    steamlining and cleanup

--- advanced.js
+++ advanced.js
@@ -1,48 +1,34 @@
-var copyContentButtons = document.getElementsByClassName("copyContentButton");
+//Global variable statement
+var statement = document.getElementById("currentStatement");
+//Adds listeners for buttons
+copyContentButtons = document.getElementsByClassName("copyContentButton"); //Direct copy buttons 
 for(i = 0; i < copyContentButtons.length; i++) {
 	copyContentButtons[i].addEventListener('click', function() {copyContentToCurrentStatement(this.textContent);}, false);
 }
-
-function copyContentToCurrentStatement(button) {
-	console.log(button);
-	var statement = document.getElementById("currentStatement")
+document.getElementById("clearStatementButton").addEventListener('click', clearStatement, false); //Clear button
+document.getElementById("evaluteStatementButton").addEventListener('click', evaluateStatement, false); //Equals button
+document.getElementById("deleteToTheLeftButton").addEventListener('click', deleteToTheLeft, false); //Del button
+//Button functions
+function copyContentToCurrentStatement(button) { //Most button functions
 	statement.appendChild(document.createTextNode(button));
 }
-
-var clearStatementButton = document.getElementById("clearStatementButton");
-clearStatementButton.addEventListener('click', clearStatement, false);
-
-function clearStatement() {
-	var statement = document.getElementById("currentStatement")
+function clearStatement() { //Clear button function
 	removeChildren(statement);
    	statement.appendChild(document.createTextNode(""));
 }
-
-var evaluteStatementButton = document.getElementById("evaluteStatementButton");
-evaluteStatementButton.addEventListener('click', evaluateStatement, false);
-
-function evaluateStatement() {
-	var statement = document.getElementById("currentStatement");
-	var answer = document.getElementById("answer")
-	var result = eval(statement.textContent);
+function evaluateStatement() { //Equals button function
+	var answer = document.getElementById("answer") 
+	result = document.createTextNode(eval(statement.textContent));
    	removeChildren(statement);
    	removeChildren(answer);
-   	answer.appendChild(document.createTextNode(result));
+   	answer.appendChild(result);
 }
-
-var deleteToTheLeftButton = document.getElementById("deleteToTheLeftButton");
-deleteToTheLeftButton.addEventListener('click', deleteToTheLeft, false);
-
-function deleteToTheLeft() {
-	var statement = document.getElementById("currentStatement");
+function deleteToTheLeft() { //Del button function
 	newStatement = statement.textContent.substring(0, statement.textContent.length-1);
 	removeChildren(statement);
 	statement.appendChild(document.createTextNode(newStatement));
 }
-
-
+//Multi use functions
 function removeChildren(item) {
-   	while (item.hasChildNodes()) {
-    	item.removeChild(item.firstChild);
-   	}
+   	while (item.hasChildNodes()) item.removeChild(item.firstChild);
 }
