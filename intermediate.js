fa777e6a7cac0ea21941aba9833a70082f138459
MrOsteoperosis <SanderVerheijen@outlook.com>
2019-01-23 11:00:08 +0100

    adds comments for intermediate.js

--- intermediate.js
+++ intermediate.js
@@ -1,35 +1,53 @@
+//Create object hotel
 var Hotel = {
-	name: "A hotel",
+	//Generate availableNights randomly between 1 and 10
     availableNights: Math.floor((Math.random()*9)+1),
+    //Create function checkAvailability with parameter nights
     checkAvailability: function(nights) {
+    	//Set availability to false
     	isAvailable = false;
+    	//Console log number of available nights
     	console.log(this.availableNights)
+    	//If there are enough nights
     	if((nights-this.availableNights)<=0) {
+    		//Set available to true
     		isAvailable = true;
     	}
+    	//Log result
     	console.log(isAvailable);
+    	//Return availability
     	return isAvailable;
     }
 }
 
-console.log(Hotel.availableNights);
-
+//Find the sumbit button
 var nightsSumbitButton = document.getElementById('nightsSumbitButton');
+//Add a listener for click to run sumbitNightsData
 nightsSumbitButton.addEventListener('click', sumbitNightsData, false);
 
+//Create function, no parameters
 function sumbitNightsData() {
+	//Find input field and store text contents (An integer hopefully)
 	var nightsNo = parseInt(document.getElementById("nightsInputField").value);
+	//run checkAvailability with nightsNo and store the value in available (True or false)
 	var available = Hotel.checkAvailability(nightsNo);
+	//Find the output container
 	var nightsOutputContainer = document.getElementById("nightsOutputContainer");
+	//Delete all child nodes to clear output space
    	while (nightsOutputContainer.hasChildNodes()) {
     	nightsOutputContainer.removeChild(nightsOutputContainer.firstChild);
    	}
 	if(available) {
+		//Create a button
 		var output = document.createElement("button")
+		//Set button text to I'll Reserve
 		output.appendChild(document.createTextNode("I'll Reserve"));
+		//Add button to output container
 		nightsOutputContainer.appendChild(output);
 	} else {
+		//Create a text node with apology
 		var output = document.createTextNode("Sorry, no free room for " + nightsNo + " nights");
+		//Add text node to output container
 		nightsOutputContainer.appendChild(output);
 	}
 }
